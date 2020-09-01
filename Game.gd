50de1cd723df685306e63b3ec9194d123f66ffa4
Juan Gaspar García <gaxdpar@gmail.com>
2020-09-01 23:20:42 +0200

    Added texts and buttons to the windows

--- Game.gd
+++ Game.gd
@@ -3,12 +3,19 @@ extends Node
 
 func _input(event: InputEvent) -> void:
 	
-	if event.is_action_pressed("exit_game"):
+	if event.is_action_pressed("exit_game") or event.is_action_pressed("ui_accept"):
 		exit_game()
-	elif event.is_action_pressed("ui_accept"):
-		for child in get_children():
-			child.closing_animation()
 
 
 func exit_game(exit_code: int = 0) -> void:
+	for child in get_children():
+		child.closing_animation()
+	for child in get_children():
+		yield(child, "closed")
+		child.queue_free()
+	
 	get_tree().quit(exit_code)
+
+
+func _on_Button_pressed() -> void:
+	exit_game()
